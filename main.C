7055c4fb48397ad9911fc80cfd1788bae5ea5a70
Aliza Nurfitrian Meizahra <alizahra2403@gmail.com>
2024-11-27 22:31:11 +0700

    Create main.C

---
+++ main.C
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Menampilkan pesan Hallo
+    cout << "Hallo! Selamat datang di sistem sensor IoT.\n\n";
+
+    // Perkenalan sensor yang akan digunakan
+    cout << "Sensor-sensor yang digunakan dalam proyek ini adalah:\n";
+    cout << "1. Sensor PIR (Passive Infrared) - Digunakan untuk mendeteksi gerakan di dalam rumah.\n";
+    cout << "2. Sensor Ultrasonik HC-SR04 - Digunakan untuk mengukur jarak objek di luar rumah.\n";
+    cout << "3. Sensor LDR (Light Dependent Resistor) - Digunakan untuk mendeteksi tingkat cahaya sekitar.\n";
+    cout << "4. Sensor Gas MQ-2 - Digunakan untuk mendeteksi kebocoran gas di sekitar rumah.\n\n";
+
+    // Pesan penutupan
+    cout << "Terima kasih telah melihat sistem sensor IoT ini!\n";
+    return 0;
+}
